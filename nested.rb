8d4088f4ef6f2c577b70b90737c738a61a166884
Melike KILIC <melike180400@gmail.com>
2019-10-20 21:29:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     } 
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
